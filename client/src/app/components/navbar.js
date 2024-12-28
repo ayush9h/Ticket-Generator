0a36509375ce31f2501c33c4b8a30baff2e64a10
Ayush Kumar <ayush.kumar.21cse@bmu.edu.in>
2024-12-28 13:18:58 +0530

    updation

--- client/src/app/components/navbar.js
+++ client/src/app/components/navbar.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Navbar() {
   return <>
-    <header className='p-5 shadow-xl bg-white'>
+    <header className='p-5 shadow-xl bg-indigo-800'>
         <nav className='max-width flex justify-between content-center'>
           <div className='flex justify-between text-center content-center'>
             <Image src='/logo.jpg' width={40} height={40} alt="dummy-image" priority />
-            <div className="text-center justify-center flex content-center">
-              <h1 className="text-xl ml-3 font-semibold mt-2">TATA MOTORS</h1>
-            </div>
+          </div>
+          <div className="text-center justify-center flex content-center">
+              <h1 className="text-xl ml-3 font-bold mt-2 text-slate-200">TATA MOTORS PASSENGER VEHICLES</h1>
           </div>
         </nav>
       </header>
