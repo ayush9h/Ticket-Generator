37ab1e05506f101b8ef39d30026751f6a54ed89d
Ayush Kumar <ayush.kumar.21cse@bmu.edu.in>
2024-12-28 21:04:32 +0530

    fixing the DB

--- client/src/app/components/loading.js
+++ client/src/app/components/loading.js
@@ -4,7 +4,7 @@ import 'react-loading-skeleton/dist/skeleton.css'
 export default function Loading() {
     return (
         <div className="max-width mt-10">
-            <SkeletonTheme baseColor='#ebeaf6' highlightColor='#9b98d1'>
+            <SkeletonTheme baseColor='#dbeafe' highlightColor='#60a5fa'>
                 <Skeleton count={1} width={1470} height={100}/>
             </SkeletonTheme>
         </div>
