bbe931f275c917c241773114e2a16e2380ec1f76
Ayush Kumar <ayush.kumar.21cse@bmu.edu.in>
2024-12-29 11:20:12 +0530

    fix

--- client/src/app/components/navbar.js
+++ client/src/app/components/navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <header className='p-5 shadow-xl bg-indigo-800'>
         <nav className='max-width flex justify-between content-center'>
           <div className='flex justify-between text-center content-center'>
-            <Image src='/logo.jpg' width={45} height={40} className="object-contain w-auto h-auto sm:w-32 sm:h-32 md:w-40 md:h-40" alt="dummy-image" priority />
+            <Image src='/logo.jpg' width={45} height={40} className="object-contain sm:w-32 sm:h-32 md:w-40 md:h-40 xl:w-10 xl:h-10" alt="dummy-image" priority />
           </div>
           <div className="text-center justify-center flex content-center">
               <h1 className="ml-3 font-bold mt-2 text-slate-200 md:text-md sm:text-sm lg:text-xl">TATA MOTORS PASSENGER VEHICLES LTD.</h1>
