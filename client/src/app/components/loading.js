48f3120550fdbeb00294344ae615001249425b8f
Ayush Kumar <ayush.kumar.21cse@bmu.edu.in>
2025-01-01 12:38:45 +0530

    fixed skeleton issue

--- client/src/app/components/loading.js
+++ client/src/app/components/loading.js
@@ -5,7 +5,7 @@ export default function Loading() {
     return (
         <div className="max-width mt-10">
             <SkeletonTheme baseColor='#dbeafe' highlightColor='#60a5fa'>
-                <Skeleton count={1} width={1470} height={100}/>
+                <Skeleton count={1} className='max-width h-14'/>
             </SkeletonTheme>
         </div>
     )
