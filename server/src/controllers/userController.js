9dfe58a65be833441cd3ffcae25399579988b911
Ayush <ayush.kumar.21cse@bmu.edu.in>
2025-08-15 15:21:09 +0530

    fix to user create

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,19 +1,18 @@
 const User = require("../models/User");
 
 exports.createUser = async (req, res) => {
-  const { userId, userMail, userName } = req.body;
+  const {userMail} = req.body;
 
   try {
     let user = await User.findOne({ userMail });
 
     if (user) {
       return res.status(200).json(user);
+    }else{
+       return res.status(400).json({ error: "User not found in the Database." });
     }
 
-    user = await User.create({ userId, userMail, userName });
-    res.status(201).json(user);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "There was an error while creating the user" });
+    res.status(500).json({ error: "There was an error while fetching the user" });
   }
 };
